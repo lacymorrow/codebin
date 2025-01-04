e863b9938bc052bc7be091fd207edabacf1092e6
r2hu1 <hi@rahul.eu.org>
2025-01-04 22:30:01 +0530

    mv

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <header className="flex justify-between py-3 border-b px-6 bg-secondary/30 backdrop-blur-lg items-center">
+      <header className="flex mb-10 justify-between py-3 border-b px-6 bg-secondary/30 backdrop-blur-lg items-center">
         <div>
           <h1 className="text-xl font-bold">Codebin</h1>
         </div>
