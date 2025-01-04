0e08feeb59df991485260bcee97a0f45709f8536
r2hu1 <hi@rahul.eu.org>
2025-01-04 22:29:26 +0530

    vwing

--- app/page.js
+++ app/page.js
@@ -1,8 +1,20 @@
 import Hero from "@/components/page/hero";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
+      <header className="flex justify-between py-3 border-b px-6 bg-secondary/30 backdrop-blur-lg items-center">
+        <div>
+          <h1 className="text-xl font-bold">Codebin</h1>
+        </div>
+        <div>
+          <Button asChild>
+            <Link href="/auth">Login</Link>
+          </Button>
+        </div>
+      </header>
       <Hero/>
     </div>
   );
