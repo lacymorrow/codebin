9763ba00973ee3d7949375ba57c84887f8fc3a5e
r2hu1 <hi@rahul.eu.org>
2025-01-06 16:17:20 +0530

    comb w/ techstack and dash img

--- components/page/hero.jsx
+++ components/page/hero.jsx
@@ -1,12 +1,21 @@
 import Link from "next/link";
 import { Button } from "../ui/button";
-import { Stars } from "lucide-react";
+import { ChevronRight, Stars, StarsIcon } from "lucide-react";
 import { RainbowButton } from "../ui/rainbow-button";
+import { BorderBeam } from "../ui/border-beam";
+import TechStack from "./techstack";
 
 export default function Hero() {
     return (
         <div className="px-6 py-10">
             <div className="grid mx-auto max-w-2xl gap-4 text-center place-items-center">
+                <div className="flex -mb-1 items-center gap-2 bg-secondary/10 border-border border rounded-md w-fit px-3 py-0.5">
+                    <StarsIcon className="h-3 w-3" />
+                    <a href="https://github.com/r2hu1/codebin" target="_blank" className="text-sm text-foreground/80 hover:underline">
+                        Give us a Star on Github
+                    </a>
+                    <ChevronRight className="h-3 w-3" />
+                </div>
                 <h1 className="font-geist text-4xl font-medium tracking-tighter bg-gradient-to-r from-zinc-800 via-stone-800/80 to-purple-800/70 dark:from-zinc-100 dark:via-stone-200/50 dark:to-purple-200/70 bg-clip-text text-transparent sm:text-5xl md:text-6xl">
                     Share code snippets with output and syntax highlighting with ease.
                 </h1>
@@ -23,6 +32,15 @@ export default function Hero() {
                         <Button variant="outline">Github <Stars className="h-4 w-4" /></Button>
                     </Link>
                 </div>
+            </div>
+            <TechStack />
+            <div className="relative rounded-md p-0.5 mt-10 max-w-[800px] mx-auto">
+                <img
+                    src="https://via.placeholder.com/800x450?text=in++development"
+                    alt="Codebin illustration"
+                    className="h-72 sm:h-full max-h-[450px] w-full object-cover rounded-md"
+                />
+                <BorderBeam borderWidth={2} />
             </div>
         </div>
     );
