3d3e8c1eb04535f8c70670ef23b2db2bf5127dbe
r2hu1 <hi@rahul.eu.org>
2025-01-06 22:32:04 +0530

    rm unc imp

--- app/page.js
+++ app/page.js
@@ -1,7 +1,6 @@
 import Footer from "@/components/page/footer";
 import Header from "@/components/page/header";
 import Hero from "@/components/page/hero";
-import TechStack from "@/components/page/techstack";
 
 export default function Home() {
   return (
