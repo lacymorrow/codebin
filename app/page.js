e833660a4abcd0c3f3957491ef90648517293c5f
r2hu1 <hi@rahul.eu.org>
2025-01-06 16:16:00 +0530

    rm tech

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
+import Footer from "@/components/page/footer";
 import Header from "@/components/page/header";
 import Hero from "@/components/page/hero";
+import TechStack from "@/components/page/techstack";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <Hero />
+      <Footer />
     </div>
   );
 }
