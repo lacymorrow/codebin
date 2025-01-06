a2f374601231468eab7fd5012ef00ce0d614c25b
r2hu1 <hi@rahul.eu.org>
2025-01-06 16:17:07 +0530

    hng

--- components/page/header.jsx
+++ components/page/header.jsx
@@ -6,10 +6,10 @@ import Logo from "../ui/logo";
 
 export default function Header() {
     return (
-        <header className="md:px-20 lg:px-32 flex mb-10 justify-between py-3 px-6 items-center">
+        <header className="relative md:px-20 lg:px-32 flex mb-10 justify-between py-3 px-6 items-center">
             <img
-                src="https://tailwindcss.com/_next/static/media/docs@30.8b9a76a2.avif"
-                className="fixed -z-50 -top-0 left-10"
+                src="/bg_gradient.avif"
+                className="absolute -z-50 -top-0"
             />
             <Logo />
             <div className="flex items-center gap-2">
