bcee40a625e2eafdf36ead221c061c9c0dbf28b9
r2hu1 <hi@rahul.eu.org>
2025-01-06 16:16:15 +0530

    mk link

--- components/ui/logo.jsx
+++ components/ui/logo.jsx
@@ -1,9 +1,12 @@
-import {AiFillCode} from "react-icons/ai";
+import Link from "next/link";
+import { AiFillCode } from "react-icons/ai";
 export default function Logo() {
     return (
-        <div className="flex items-center gap-1">
-            <h1 className="text-xl font-bold">Codebin</h1>
-            <AiFillCode className="h-5 w-5" />
-        </div>
+        <Link href="/">
+            <div className="flex items-center gap-1">
+                <h1 className="text-xl font-bold">Codebin</h1>
+                <AiFillCode className="h-5 w-5" />
+            </div>
+        </Link>
     )
 }
