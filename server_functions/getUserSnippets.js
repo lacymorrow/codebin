83b157cdb7b0d2fafa5fd193e9dd73fd9e0e495a
r2hu1 <hi@rahul.eu.org>
2025-01-10 12:14:52 +0530

    inc. id

--- server_functions/getUserSnippets.js
+++ server_functions/getUserSnippets.js
@@ -4,14 +4,14 @@ import { db } from "@/lib/firebase";
 import { collection, getDocs, doc } from "firebase/firestore";
 
 export default async function getUserSnippets(id) {
-    const userDocRef = doc(db, 'users', id);
-    const snippetsCollectionRef = collection(userDocRef, 'snippets');
-
     try {
+        const userDocRef = doc(db, 'users', id);
+        const snippetsCollectionRef = collection(userDocRef, 'snippets');
         const snippetsSnapshot = await getDocs(snippetsCollectionRef);
-        const snippetsList = snippetsSnapshot.docs.map(doc => doc.data());
+        const snippetsList = snippetsSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
         return snippetsList;
     } catch (error) {
-        console.error(error);
+        console.error("Error fetching snippets:", error);
+        return [];
     }
 }
