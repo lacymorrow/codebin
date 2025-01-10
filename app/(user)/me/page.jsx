2925c3abdb091648f6ecc6d1a9cf9af6106274a1
r2hu1 <hi@rahul.eu.org>
2025-01-10 12:03:23 +0530

    func

--- app/(user)/me/page.jsx
+++ app/(user)/me/page.jsx
@@ -4,19 +4,58 @@ import { Button } from '@/components/ui/button';
 import { Skeleton } from '@/components/ui/skeleton';
 import { Switch } from '@/components/ui/switch';
 import createSnippet from '@/server_functions/createSnippet';
+import getUserSnippets from '@/server_functions/getUserSnippets';
 import { getCurrentUser } from '@/utils/current-user';
-import { Share, Share2 } from 'lucide-react';
+import { Copy, Edit, Share, Share2 } from 'lucide-react';
 import { useEffect, useState } from 'react';
 
 const UserProfile = () => {
     const [user, setUser] = useState(null);
+    const [snippets, setSnippets] = useState([]);
+
+    const fetchSnippets = async () => {
+        if (!user) return;
+        try {
+            const snippets = await getUserSnippets(user.uid);
+            setSnippets(snippets);
+            console.log(snippets);
+        } catch (error) {
+            console.error('Error fetching snippets:', error);
+        }
+    };
 
     useEffect(() => {
         getCurrentUser(setUser);
     }, []);
+    useEffect(() => {
+        fetchSnippets();
+    }, [user]);
 
     if (!user) {
-        return <p>Loading or no user logged in...</p>;
+        return (
+            <div className='px-6 md:px-20 lg:px-32'>
+                <div className='flex gap-4'>
+                    <Skeleton className='w-24 h-24' />
+                    <div className='h-full grid gap-2'>
+                        <Skeleton className='w-36 h-4' />
+                        <div className='flex items-center gap-2'>
+                            <Skeleton className="h-5 w-8" />
+                            <Skeleton className="h-5 w-8" />
+                            <Skeleton className="h-5 w-8" />
+                        </div>
+                        <Skeleton className='h-9 w-32' />
+                    </div>
+                </div>
+                <div className='mt-14 grid gap-4 mb-10'>
+                    <h1 className='text-base text-foreground/80'>My Snippets.</h1>
+                    <div className='grid grid-cols-2 sm:grid-cols-3 gap-3'>
+                        {[...Array(10)].map((_, i) => (
+                            <Skeleton key={i} className='w-full sm:max-w-48 h-48' />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        );
     }
 
     return (
@@ -37,11 +76,31 @@ const UserProfile = () => {
             </div>
             <div className='mt-14 grid gap-4 mb-10'>
                 <h1 className='text-base text-foreground/80'>My Snippets.</h1>
-                <div className='grid grid-cols-2 sm:grid-cols-3 gap-3'>
-                    {[...Array(10)].map((_, i) => (
-                        <Skeleton key={i} className='w-full sm:max-w-48 h-48' />
+                <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 auto-rows-auto">
+                    {snippets.map((snippet) => (
+                        <div
+                            key={snippet.id}
+                            className="border flex flex-col border-border h-fit bg-card rounded-md p-4 shadow-sm break-inside-avoid"
+                        >
+                            <div className="bg-secondary rounded-md p-2 mb-3 overflow-hidden break-words max-h-[150px] min-h-[150px]">
+                                <p className="text-sm text-foreground/80 overflow-hidden break-words">
+                                    {snippet.code}
+                                </p>
+                            </div>
+                            <div className="grid px-1 h-fit">
+                                <h1 className="text-base font-medium">{snippet.title}</h1>
+                                <p className="text-sm text-foreground/80">
+                                    {snippet.desc ? snippet.desc.slice(0, 70) : "No description"}
+                                </p>
+                                <div className='flex gap-2 items-center mt-2'>
+                                    <Button>Copy <Copy className='h-4 w-4' /></Button>
+                                    <Button size="icon" variant="outline"><Share2 className='h-4 w-4' /></Button>
+                                </div>
+                            </div>
+                        </div>
                     ))}
                 </div>
+
             </div>
         </div>
     );
