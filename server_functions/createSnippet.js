1f762dba52319e419002ac4a8f0607f1afbe3aa4
r2hu1 <hi@rahul.eu.org>
2025-01-11 11:53:32 +0530

    mod to make new not sub

--- server_functions/createSnippet.js
+++ server_functions/createSnippet.js
@@ -1,19 +1,17 @@
 "use server";
 
 import { db } from "@/lib/firebase";
-import { getCurrentUser } from "@/utils/current-user";
-import { addDoc, collection, doc } from "firebase/firestore";
+import { addDoc, collection } from "firebase/firestore";
 
 export default async function createSnippet(id, dataa) {
-    const userDocRef = doc(db, 'users', id);
-    const snippetsCollectionRef = collection(userDocRef, 'snippets');
+    const snippetsCollectionRef = collection(db, 'snippets'); // Root-level 'snippets' collection
 
     try {
-        const newSnippetRef = await addDoc(snippetsCollectionRef, dataa);
+        const newSnippetRef = await addDoc(snippetsCollectionRef, { author: id, ...dataa });
         console.log('Snippet created with ID:', newSnippetRef.id);
         return newSnippetRef.id;
     } catch (error) {
         console.error('Error adding snippet:', error);
+        throw error;
     }
-
 }
